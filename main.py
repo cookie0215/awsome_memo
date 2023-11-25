2478e575c6e9dda32fcd41ff7b79ad33e01cdaa1
cookie0215 <81572770+cookie0215@users.noreply.github.com>
2023-11-25 20:14:47 +0900

    feat: 메모 앱을 통해 CRUD 구현 연습

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
 class Memo(BaseModel):
-  id:int
+  id:str
   content:str
 
 memos = []
@@ -19,5 +19,21 @@ def create_memo(memo:Memo):
 @app.get("/memos")
 def read_memo():
   return memos
+
+@app.put("/memos/{memo_id}")
+def put_memo(req_memo:Memo):
+  for memo in memos:
+    if memo.id == req_memo.id:
+        memo.content= req_memo.content
+        return '수정 성공했습니다.'
+  return '수정할 메모가 존재하지 않습니다.'
+
+@app.delete("/memos/{memo_id}")
+def delete_memo(memo_id):
+  for index, memo in enumerate(memos):
+    if memo.id == memo_id:
+        memos.pop(index)
+        return '삭제 성공했습니다.'
+  return '삭제될 메모가 존재하지 않습니다.'
 
 app.mount("/", StaticFiles(directory='static', html=True), name='static')
--- static/app.js
+++ static/app.js
@@ -1,17 +1,65 @@
 /* 
-<메모 생성하기>
+<메모 생성, 수정, 삭제하기>
 1. input창에 내용 입력
 2. '생성' 버튼 클릭
 3. form에 입력한 값이 제출(전송)
 4. input값에 입력한 내용은 지워져야 됨
 5. 동시에 입력한 메모가 하단에 업데이트될 수 있도록 서버에게 요청을 보내야 한다. (fetch 사용 - 단, 이때 값을 update시켜야 하기 때문에 post요청을 해야 한다.)
 6. 서버에 있는 값을 받아서 읽어줘야 한다. (get요청으로 값을 달라고 해야 함)
+7. 이미 작성된 메모를 수정하기 위해 '수정하기'버튼을 만들어야 한다.
+8. 메모를 삭제할 수 있는 버튼 만들기
 */
+
+
+async function editMemo(event) {
+  const id = event.target.dataset.id;
+  const editInput = prompt("수정할 내용을 입력하세요");
+
+  // 수정하려는 내용을 서버에 요청
+  const res = await fetch(`/memos/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: id,
+      content: editInput,
+    }),
+  });
+
+  readMemo();
+}
+
+async function deleteMemo(event) {
+  const id = event.target.dataset.id;
+
+  // 삭제하려는 내용을 서버에 요청
+  const res = await fetch(`/memos/${id}`, {
+    method: "DELETE",
+  });
+
+  readMemo();
+}
 
 function displayMemos(memo) {
   const ul = document.querySelector("#memo-ul");
   const li = document.createElement("li");
-  li.innerText = `id:${memo.id}, content:${memo.content}`;
+  const editBtn = document.createElement("button");
+  const deleteBtn = document.createElement("button");
+
+  li.innerText = memo.content;
+
+  editBtn.innerText = "수정";
+  editBtn.addEventListener('click', editMemo);
+  editBtn.dataset.id = memo.id;
+
+  deleteBtn.innerText = "삭제";
+  deleteBtn.addEventListener('click', deleteMemo);
+  deleteBtn.dataset.id = memo.id;
+
+
+  li.appendChild(editBtn);
+  li.appendChild(deleteBtn);
   ul.appendChild(li);
 }
 
@@ -31,7 +79,7 @@ async function createMemo(value) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      id: new Date().getTime(),
+      id: new Date(),
       content: value,
     }),
   });
